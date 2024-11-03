6d5886cf63eed0c8afdc044decf77518b74d62f9
raphael <raphael.freitas.pessoal@gmail.com>
2024-11-03 02:37:32 -0300

    primeiro comit

---
+++ front/src/router/AppRouter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Header from '../components/Header';
+import AddBook from '../components/AddBook';
+import BooksList from '../components/BooksList';
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Header />
+        <div className="main-content">
+          <Routes>
+            <Route element={<BooksList/>} path="/" exact="true" />
+            <Route element={<AddBook/>} path="/add"  />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
